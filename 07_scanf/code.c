267863dafc8ebf506a46568126b63b976def21ba
CJM1994 <cmullin@ingenio.ca>
2024-01-19 23:09:49 -0700

    practice work

--- 07_scanf/code.c
+++ 07_scanf/code.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-void print_fraction_addition() {
-  // TEST: PROGRAM: ADDING FRACTIONS
+void program_adding_fractions() {
   int num1, denom1, num2, denom2, result_num, result_denom;
 
   printf("Enter 2 fractions to add separated by a space: \n");
@@ -13,15 +12,7 @@ void print_fraction_addition() {
   printf("The resulting fraction is: %d/%d", result_num, result_denom);
 }
 
-int main() {
-  // Testing output
-  printf("%6d,%4d\n", 86, 1040);
-  printf("%12.5e\n", 30.253);
-  printf("%.4f\n", 83.162);
-  printf("%-6.2g\n", .0000009979);
-
-  // TEST: EXERCISE DISPLAY FLOATS
-  //
+void exercise_3_1() {
   // Exponential notation; left-justified in a field of size 8; one digit after
   // the decimal point.
   printf("|%-8.1e|\n", 5.0);
@@ -34,6 +25,42 @@ int main() {
   // Fixed decimal notation; right-justified in a field of size 6; no digits
   // after the decimal point.
   printf("|%6.0f|\n", 2.3);
+}
 
-  print_fraction_addition();
+void section_3_2() {
+  int i, j;
+  float x;
+  scanf("%d%f%d", &i, &x, &j);
+  printf("%d %f %d\n", i, x, j);
+  // If the user enters
+  // 10.3 5 6
+  // what will be the values of i, x, and j after the call? (Assume that i and j
+  // are int variables and x is a float variable.)
+  // Answer: should fill the first int with 10, then hit the decimal and move to
+  // the float which will be .3 until it hits whitespace and moves on to the
+  // last int which will be 5, then it will hit the whitespace and 6 will sit in
+  // the buffer for a future printf or will need to be cleared
+
+  // For each of the following pairs of scanf format strings, indicate whether
+  // or not the two strings are equivalent. If they’re not. show how they can be
+  // distinguished.
+  // (a) "%d" versus " %d"
+  // Should be equivalent
+  // (b) "%d-%d-%d'' versus "%d -%d -%d"
+  // No, this would fail
+  // (c) "%f" versus "%f "
+  // No, this would require an extra input at the end to run
+  // (d) "%f,%f, versus "%f, %f"
+  // Yes, these should be equivalent
+  float a, b;
+  int c;
+
+  scanf("%f%d%f", &a, &c, &b);
+  printf("%f %d %f\n", a, c, b);
+}
+
+int main() {
+  exercise_3_1();
+  section_3_2();
+  program_adding_fractions();
 }
