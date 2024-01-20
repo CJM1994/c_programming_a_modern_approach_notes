5f91c3d84e44a8fea35ed5493968dbccee40aae6
CJM1994 <cmullin@ingenio.ca>
2024-01-19 22:44:38 -0700

    add printf practice

--- 07_scanf/code.c
+++ 07_scanf/code.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void print_fraction_addition() {
+  // TEST: PROGRAM: ADDING FRACTIONS
   int num1, denom1, num2, denom2, result_num, result_denom;
 
   printf("Enter 2 fractions to add separated by a space: \n");
@@ -12,4 +13,27 @@ void print_fraction_addition() {
   printf("The resulting fraction is: %d/%d", result_num, result_denom);
 }
 
-int main() { print_fraction_addition(); }
+int main() {
+  // Testing output
+  printf("%6d,%4d\n", 86, 1040);
+  printf("%12.5e\n", 30.253);
+  printf("%.4f\n", 83.162);
+  printf("%-6.2g\n", .0000009979);
+
+  // TEST: EXERCISE DISPLAY FLOATS
+  //
+  // Exponential notation; left-justified in a field of size 8; one digit after
+  // the decimal point.
+  printf("|%-8.1e|\n", 5.0);
+  // Exponential notation; right-justified in a field of size 10; six digits
+  // after the decimal point.
+  printf("|%10e|\n", 7.123456789);
+  // Fixed decimal notation; left-justified in a field of size 8; three digits
+  // after the decimal point.
+  printf("|%-8.3f|\n", 2.234);
+  // Fixed decimal notation; right-justified in a field of size 6; no digits
+  // after the decimal point.
+  printf("|%6.0f|\n", 2.3);
+
+  print_fraction_addition();
+}
